4b06b32732c7ef3de6592799ccd3bb2ab1d0d425
Leandro <leomoralesarce@gmail.com>
2022-04-20 18:32:48 -0300

    create / route

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
+const characters = require("./db");
+
+app.get("/", (request, response) => {
+  response.send({
+    status: 200,
+    msg: "Bienvenidos a la API REST de Breaking Bad",
+  });
+});
 
 app.listen(3000, () => console.log("server listening on port 3000"));
