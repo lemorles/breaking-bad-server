8adbd0268dd88ae2d56d33914fc688187fa8de3e
Leandro <leomoralesarce@gmail.com>
2022-04-20 18:33:59 -0300

    create /characters/:id route

--- index.js
+++ index.js
@@ -13,4 +13,17 @@ app.get("/characters", (request, response) => {
   response.json(characters);
 });
 
+app.get("/characters/:id", (request, response) => {
+  // const id = request.params.id;
+  const { id } = request.params;
+
+  const found = characters.find((c) => c.char_id === parseInt(id));
+
+  if (found) {
+    return response.json(found);
+  }
+
+  response.json({ msg: "Character not found" });
+});
+
 app.listen(3000, () => console.log("server listening on port 3000"));
