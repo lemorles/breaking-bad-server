33c29b0a51436a37c56e0a34f7adbddd0c87f637
Leandro <leomoralesarce@gmail.com>
2022-04-22 19:17:37 -0300

    add morgan config

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const characters = require("./db");
+
+app.use(morgan("dev"));
 
 app.get("/", (request, response) => {
   response.send({
